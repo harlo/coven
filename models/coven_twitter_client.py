fda432a9fe06dc29012da88d638f86405ed4fe77
harlo <harlo.holmes@gmail.com>
2015-03-17 21:59:50 -0400

    reorg twitter initiation

--- models/coven_twitter_client.py
+++ models/coven_twitter_client.py
@@ -6,31 +6,12 @@ class CovenTwitterClient(twitter.Api):
 		if DEBUG:
 			print "twitter client..."
 
-		c_keys = ["ACCESS_TOKEN_KEY", "ACCESS_TOKEN_SECRET", "CONSUMER_SECRET", "CONSUMER_KEY"]
-		ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_SECRET, CONSUMER_KEY = get_config(["TWITTER_%s" % k for k in c_keys])
-
 		credentials = {}
-		missing_credentials = []
+		for t in ["ACCESS_TOKEN_KEY", "ACCESS_TOKEN_SECRET", "CONSUMER_SECRET", "CONSUMER_KEY"]:
+			credentials[t.lower()] = get_config("TWITTER_%s" % t)
 
 		try:
-			for c in c_keys:
-				if locals(c) is None:
-					missing_credentials.append(c)
-
-				credentials[c.lower()] = locals[c]
-
-			if len(missing_credentials) > 0:
-				if not __init_twitter_account():
-					if DEBUG:
-						print "could not make twiiter happen."
-
-					return
-
-				for c in missing_credentials:
-					locals[c] = get_config("TWITTER_%s" % c)
-					credentials[c.lower()] = locals[c]
-
-			twitter.Api.__init(self, **credentials)
+			twitter.Api.__init__(self, **credentials)
 		except Exception as e:
 			if DEBUG:
 				print e, type(e)
@@ -40,81 +21,19 @@ class CovenTwitterClient(twitter.Api):
 		if lookup is not None:
 			self.lookup = lookup
 
-	def __init_twitter_account():
-		if DEBUG:
-			print "init twitter account"
-
-		authorization_url = "http://api.twitter.com/oauth/authorize"
-		request_token_url = "http://api.twitter.com/oauth/request_token"
-		access_token_url = "http://api.twitter.com/oauth/access_token"
-
-		try:
-			consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-			client = oauth2.Client(consumer)
-		except Exception as e:
-			if DEBUG:
-				print e, type(e)
-
-			return False
-
-		res, content = client.request(request_token_url, "POST", body="oauth_callback=oob")
-		if int(res['status']) != 200:
-			if DEBUG:
-				print "no, invalid response from twtter: %s" % res['status']
-
-			return False
-
-		import urlparse
-		from fabric.operations import prompt
-
-		token_request = dict(urlparse.parse_qsl(content))
-
-		if DEBUG:
-			print "TOKEN REQUEST:"
-			print token_request
-
-		print "Visit the following URL in a browser.  Come back and paste in the PIN here."
-		print "*****"
-		print "%s?oauth_token=%s" % (authorization_url, token_request['oauth_token'])
-		print "*****"
-		oauth_pin = prompt("PIN: ")
-
-		try:
-			token = oauth2.Token(token_request['oauth_token'], token_request['oauth_token_secret'])
-			token.set_verifier(oauth_pin)
-			client = oauth2.Client(consumer, token)
-
-			res, content = client.request(access_token_url, "POST")
-			access_token = dict(urlparse.parse_qsl(content))
-
-			if DEBUG:
-				print "TOKEN CREATED:"
-				print access_token
-
-			return __append_to_config({
-				'TWITTER_CONSUMER_SECRET' : access_token['oauth_token_secret'],
-				'TWITTER_CONSUMER_KEY' : access_token['oauth_token']
-			})
-
-		except Exception as e:
-			if DEBUG:
-				print e, type(e)
-
-		return False
-
 	def get_chal_response(self):
 		if DEBUG:
 			print "get chal response"
 
-		# do whatever to trigger a chal-response and then
+		# XXX: do whatever to trigger a chal-response and then
 		# self.lookup.publish('CHAL_RESPONSE_QUEUE', chal_response)
 
-	def send_tweet(self, message):
+	def send_tweet(self, tweet):
 		if DEBUG:
 			print "send tweet"
-			print message
+			print tweet
 
-		res = self.PostUpdate(message)
+		res = self.PostUpdate(tweet)
 		
 		if DEBUG:
 			print res
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from utils.conf import DEBUG, BASE_DIR, DUtilsKey, append_to_config, build_confi
 CEREMONY_PROMPT = "how do you enter the circle?"
 CEREMONY_RESPONSE = "with perfect love and perfect trust"
 COVEN_SENTINEL = "D-D-DEFAULT OOPS"
+JABBER_SERVER = "talk.google.com"
 
 def __setup_redis():
 	import re
@@ -53,9 +54,8 @@ def __setup_redis():
 def __setup_vars(with_config):
 	MONITOR_ROOT = os.path.join(BASE_DIR, ".monitor")
 
-	return append_to_config({
+	res, config = append_to_config({
 		'PURPLE_OPTS' : {
-			'connect_server' : "talk.google.com",
 			'port' : '443',
 			'old_ssl' : True
 		},
@@ -72,24 +72,91 @@ def __setup_vars(with_config):
 		}
 	}, with_config=with_config)
 
-def __setup_coven(with_config):
-	global COVEN_PROMPT, COVEN_SENTINEL
+	if res:
+		config['PURPLE_OPTS']['connect_server'] = config['JABBER_SERVER']
+		del config['JABBER_SERVER']
 
+		return save_config(config, with_config=with_config)
+
+	return False
+
+def __setup_twitter(consumer_key, consumer_secret):
+	authorization_url = "http://api.twitter.com/oauth/authorize"
+	request_token_url = "http://api.twitter.com/oauth/request_token"
+	access_token_url = "http://api.twitter.com/oauth/access_token"
+
+	import oauth2
+
+	try:
+		consumer = oauth2.Consumer(consumer_key, consumer_secret)
+		client = oauth2.Client(consumer)
+	except Exception as e:
+		if DEBUG:
+			print e, type(e)
+
+		return False
+
+	res, content = client.request(request_token_url, "POST", body="oauth_callback=oob")
+	if int(res['status']) != 200:
+		if DEBUG:
+			print "no, invalid response from twtter: %s" % res['status']
+
+		return False
+
+	import urlparse
+	from fabric.operations import prompt
+
+	token_request = dict(urlparse.parse_qsl(content))
+
+	if DEBUG:
+		print "TOKEN REQUEST:"
+		print token_request
+
+	print "Visit the following URL in a browser.  Come back and paste in the PIN here."
+	print "*****"
+	print "%s?oauth_token=%s" % (authorization_url, token_request['oauth_token'])
+	print "*****"
+	oauth_pin = prompt("PIN: ")
+
+	try:
+		token = oauth2.Token(token_request['oauth_token'], token_request['oauth_token_secret'])
+		token.set_verifier(oauth_pin)
+		client = oauth2.Client(consumer, token)
+
+		res, content = client.request(access_token_url, "POST")
+		return dict(urlparse.parse_qsl(content))
+
+	except Exception as e:
+		if DEBUG:
+			print e, type(e)
+
+	return None
+
+def __setup_coven(with_config):
 	conf_keys = [
 		DUtilsKey("COVEN_SENTINEL", "Set your Coven's sentinel. This passphrase is used by members to request special features.",
 			COVEN_SENTINEL, COVEN_PROMPT, None),
 		DUtilsKey("CEREMONY_PROMPT", "Set your Ceremony's call phrase.",
 			CEREMONY_PROMPT, CEREMONY_PROMPT, None),
 		DUtilsKey("CEREMONY_RESPONSE", "Now, set the response to that call phrase.",
-			CEREMONY_RESPONSE, CEREMONY_RESPONSE, None)
+			CEREMONY_RESPONSE, CEREMONY_RESPONSE, None),
+		DUtilsKey("JABBER_SERVER", "What is the hostname of your Jabber server?",
+			JABBER_SERVER, JABBER_SERVER, None)
 	]
+
+	for t in ["ACCESS_TOKEN_KEY", "ACCESS_TOKEN_SECRET", "CONSUMER_KEY", "CONSUMER_SECRET"]:
+		conf_keys.append(DUtilsKey("TWITTER_%s" % t, "Twitter %s" % t.lower().replace("_", ""),
+			"none", "none", None))
 
 	try:
 		config = build_config(conf_keys, with_config)
-		
-		print config
+		if not save_config(config, with_config=with_config):
+			return False
 
-		return save_config(config, with_config=with_config)
+		twitter_credentials = __setup_twitter(config['TWITTER_CONSUMER_KEY'], config['TWITTER_CONSUMER_SECRET'])
+
+		if twitter_credentials is not None:
+			return append_to_config({'TWITTER_OAUTH_CREDENTIALS' : twitter_credentials }, with_config=with_config)
 
 	except Exception as e:
 		if DEBUG:
